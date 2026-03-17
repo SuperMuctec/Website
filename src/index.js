b6bd2877db45e7b2851594bd78ef76631b6669d8
Supermuctec <supermutec@gmail.com>
2026-03-18 02:54:49 +0530

    Added tailwind, react router support + added navbar placeholder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Route } from 'react-router-dom';
+import Home from './Pages/Home';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
